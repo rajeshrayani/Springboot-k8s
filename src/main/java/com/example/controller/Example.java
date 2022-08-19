b7aac8af1dbdec6a024f50554de68b80fefca8c2
Rajesh Rayani <65596407+rajeshrayani@users.noreply.github.com>
2022-08-19 19:26:49 +0530

    fin

--- src/main/java/com/example/controller/Example.java
+++ src/main/java/com/example/controller/Example.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
 	@RequestMapping("/")
     public String sayHello() {
-		return "<h2>Hello This is my First Web</h2>";
+		return "<h2>Hello</h2>";
 	}
 
 
